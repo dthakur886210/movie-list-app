3d156039fa5557f67034bd40ed2dad7134877fb8
dthakur886210 <97616842+dthakur886210@users.noreply.github.com>
2022-04-08 01:07:23 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React,{useState} from 'react';
+import NewMovie from './components/newMovie'
+import './App.css';
+
+function App() {
+return(
+<>
+<NewMovie/>
+    </>
+  );
+}
+export default App;
